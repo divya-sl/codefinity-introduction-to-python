35955168b2a5943c52ce08e45ccbe4e98b5276fc
divya-sl <>
2025-12-16 21:55:56 +0000

    Challenge: Pricing Adjustment Capstone

--- other_data_types/challenge_pricing_adjustment_capstone/main.py
+++ other_data_types/challenge_pricing_adjustment_capstone/main.py
@@ -9,7 +9,7 @@ egg_details = grocery_inventory.get("Eggs")
 if(egg_details[1] > 5):
     print("Eggs are too expensive, reducing the price by $1.")
     grocery_inventory["Eggs"] = ("Diary", 4.50, 30)
-    print(f"updated egg price: {grocery_inventory.get("Eggs")[1]}")
+    #print(f"updated egg price: {grocery_inventory.get("Eggs")[1]}")
 else:
     print("The price of Eggs is reasonable.")
 
@@ -20,7 +20,7 @@ milk_details = grocery_inventory.get("Milk")
 if(milk_details[2] < 10):
     print("Milk needs to be restocked. Increasing stock by 20 units.")
     grocery_inventory["Milk"] = ("Diary", 3.50, 28)
-    print(f"updated milk details: {grocery_inventory.get("Milk")[2]}")
+    #print(f"updated milk details: {grocery_inventory.get("Milk")[2]}")
 else:
     print("Milk had sufficient stock.")
 
